43ef2586c41b73f461697d17869a0bc1a4f4595a
Saleem-18 <97940760+Saleem-18@users.noreply.github.com>
2023-01-14 04:35:09 +0500

    final commit

--- app/src/main/java/com/example/myquranapp/MainActivity.java
+++ app/src/main/java/com/example/myquranapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.myquranapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     TextView txt_parah;
     TextView txt_display;
     Button btn_search;
+    Button btn_git;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +25,15 @@ public class MainActivity extends AppCompatActivity {
         txt_parah=findViewById(R.id.text_parah);
         txt_display= findViewById(R.id.textView_display);
         btn_search=findViewById(R.id.btn_search);
+        btn_git=findViewById(R.id.btn_git);
 
         String ayat = new QuranArabicText().QuranArabicText[0];
+        btn_git.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                golink("https://github.com/Saleem-18/myQURANapp.git");
+            }
+        });
         btn_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,5 +43,10 @@ public class MainActivity extends AppCompatActivity {
                 txt_display.setText(new QuranArabicText().QuranArabicText[new QDH().getParahStart(parahNumber) + surahNumber]);
             }
         });
+    }
+
+    private void golink(String s) {
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
     }
 }
